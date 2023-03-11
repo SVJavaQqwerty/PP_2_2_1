5989eeeffe8498434011356123da78808341ae78
пк <mr.setevik@rambler.ru>
2023-03-11 10:17:12 +0300

    Работает

--- src/main/java/hiber/model/User.java
+++ src/main/java/hiber/model/User.java
@@ -1,5 +1,7 @@
 package hiber.model;
 
+import org.hibernate.annotations.Cascade;
+
 import javax.persistence.*;
 
 @Entity
@@ -19,16 +21,30 @@ public class User {
    @Column(name = "email")
    private String email;
 
+   @OneToOne
+   @JoinColumn(name = "car_id")
+   @Cascade(org.hibernate.annotations.CascadeType.ALL)
+   private Car car;
+
    public User() {}
    
    public User(String firstName, String lastName, String email) {
       this.firstName = firstName;
       this.lastName = lastName;
       this.email = email;
+
+   }
+
+   public Car getCar() {
+      return car;
+   }
+
+   public void setCar(Car car) {
+      this.car = car;
    }
 
    public Long getId() {
-      return id;
+      return this.id;
    }
 
    public void setId(Long id) {
